e96c98e9e275fc5078d2322a8cc96c676ad0ff0b
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-01-19 21:50:50 -0500

    added og image and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,54 @@
-import { GeistSans } from 'geist/font/sans'
-import './globals.css'
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+  : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
-}
+  title: "UWClubs",
+  description: "A calendar for club events",
+  openGraph: {
+    title: "UWClubs",
+    description: "A calendar for club events",
+    url: defaultUrl,
+    siteName: "UWClubs",
+    images: [
+      {
+        url: `${defaultUrl}/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "UWClubs",
+      },
+      {
+        url: `${defaultUrl}/twitter-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "UWClubs",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
+      <head>
+        <link rel="icon" href="favicon.ico" />
+      </head>
+
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import Rive from "@rive-app/react-canvas";
 export default async function Index() {
 
   return (
     <div>
+      <div style={{ width: 200, height: 200 }}>
+      <Rive src="/mantis.riv" />
+      </div>
     </div>
-  )
+  );
 }
