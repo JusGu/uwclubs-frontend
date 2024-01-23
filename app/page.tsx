9beb835545e307b8b5b146efa2090555f41264c6
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-01-23 15:22:01 -0500

    changed frontpage hero size

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 "use client"
-import Rive from "@rive-app/react-canvas";
+import Rive, { Layout, Fit, Alignment } from '@rive-app/react-canvas';
 import { LucideArrowDownCircle } from "lucide-react";
 export default async function Index() {
 
   return (
     <div className="flex flex-col items-center min-h-screen w-full h-full" style={{ backgroundColor: "#255270" }}>
-      <div style={{ backgroundColor: "#255270", width: 500, height: 300 }}>
-      <Rive src="/hero.riv" />
+      <div className="flex flex-col w-4/5 md:w-1/2 h-screen items-stretch justify-center">
+      <Rive src="/hero.riv"
+       style={{ width: '100%' }}
+       layout={new Layout({ fit: Fit.FitWidth, alignment: Alignment.Center })}
+       />
       </div>
-      <div className="relative animate-bounce" style={{ color: "#255270" }}>
+      {/* <div className="relative animate-bounce" style={{ color: "#255270" }}>
         <LucideArrowDownCircle size={48} color="#F0EBD8" className="animate-fade-in" />
-      </div>
+      </div> */}
     </div>
   );
 }
