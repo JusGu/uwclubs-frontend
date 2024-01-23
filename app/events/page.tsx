1a619beeeffca61b05c6d940fd02c33923a7f8bb
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-01-23 17:14:06 -0500

    shifted timeline to left

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -5,7 +5,7 @@ import EventPageTitle from "@/components/events/EventPageTitle";
 export default function Page() {
   return (
     <div className="w-full">
-      <div className="w-full sm:w-1/2 p-6 mx-auto">
+      <div className="w-full sm:w-1/2 p-6 mx-auto sm:max-w-2xl">
         <EventPageTitle />
         <div className="flex w-full justify-items-stretch">
           <Timeline />
