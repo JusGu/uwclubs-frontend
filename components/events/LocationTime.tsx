237c5a9a125cdee5e1ba335f39fdb39215cad651
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-01 17:09:56 -0500

    fixed alert glitch

--- components/events/LocationTime.tsx
+++ components/events/LocationTime.tsx
@@ -1,43 +1,36 @@
-import { Calendar, MapPin } from "lucide-react";
+import { Clock, MapPin } from "lucide-react";
 import { Alert, AlertTitle, AlertDescription } from "../ui/alert";
 import { event } from "../../app/events/models";
 
 const formatTime = (time: string) => {
-    const date = new Date(time);
-    return date.toLocaleTimeString("en-US", {
-      timeZone: "EST",
-      hour12: true,
-      hour: "numeric",
-      minute: "numeric",
-    });
-  };
-
-  const getMonthDate = (time: string) => {
-    const date = new Date(time);
-    return (
-      date.toDateString().split(" ")[1] +
-      " " +
-      date.toDateString().split(" ")[2]
-    );
-  };
+  const date = new Date(time);
+  return date.toLocaleTimeString("en-US", {
+    timeZone: "EST",
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  });
+};
 
 export default function LocationTime({ event }: { event: event }) {
-    return (
-        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-x-2 sm:space-y-0">
+  return (
+    <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-x-2 sm:space-y-0">
+      <Alert className="w-full">
+        {/* <AlertTitle>{getMonthDate(event.start_time)}</AlertTitle> */}
+        <div className="flex items-center gap-3">
+          <Clock className="h-5 w-5" />
+          {formatTime(event.start_time)} - {formatTime(event.end_time)}
+        </div>
+      </Alert>
+      {event.location && (
         <Alert className="w-full">
-          <Calendar className="h-4 w-4" />
-          {/* <AlertTitle>{getMonthDate(event.start_time)}</AlertTitle> */}
-          <AlertDescription>
-            {formatTime(event.start_time)} - {formatTime(event.end_time)}
-          </AlertDescription>
-        </Alert >
-        {event.location && (
-          <Alert className="w-full flex items-center">
-            <MapPin className="h-4 w-4" />
-            {/* <AlertTitle>Location</AlertTitle> */}
-            <div>{event.location}</div>
-          </Alert>
-        )}
-      </div>
-    )
+          {/* <AlertTitle>Location</AlertTitle> */}
+          <div className="flex items-center gap-3">
+            <MapPin className="h-5 w-5" />
+            {event.location}
+          </div>
+        </Alert>
+      )}
+    </div>
+  );
 }
