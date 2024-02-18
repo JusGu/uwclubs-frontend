b0da3f3c57cbdf51665c1bf23077998e91cb5751
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-18 14:37:37 -0500

    added more console logs

--- lib/getWeekRange.ts
+++ lib/getWeekRange.ts
@@ -1,12 +1,16 @@
 export function getWeekRange(date: Date) {
+  console.log(`Input date: ${date.toISOString()}`);
   const dayOfWeek = date.getDay();
+  console.log(`Day of week: ${dayOfWeek}`);
   const startOfWeek = new Date(date);
   startOfWeek.setDate(date.getDate() - dayOfWeek);
   startOfWeek.setHours(0, 0, 0, 0); // Start of the day
+  console.log(`Start of week: ${startOfWeek.toISOString()}`);
 
   const endOfWeek = new Date(startOfWeek);
   endOfWeek.setDate(startOfWeek.getDate() + 6);
   endOfWeek.setHours(23, 59, 59, 999); // End of the day
+  console.log(`End of week: ${endOfWeek.toISOString()}`);
 
   return { startOfWeek, endOfWeek };
 }
