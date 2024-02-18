57f58ecb86acb86c1a15a1690711b98c44aa571d
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-18 13:39:50 -0500

    added console log to track deviation in utc

--- lib/utils.ts
+++ lib/utils.ts
@@ -21,8 +21,11 @@ export function organizeEventsByDate(data: any): IWeeklyEvents {
   const events: IWeeklyEvents = {};
 
   data.forEach((event: any) => {
+    console.log(event.start_time, "Event Start Time");
     const date = zonedTimeToUtc(event.start_time, "America/New_York");
+    console.log(date, "Date");
     const dateKey = formatInTimeZone(date, "America/New_York", "yyyy-MM-dd");
+    console.log(dateKey, "Date Key");
     if (!events[dateKey]) {
       events[dateKey] = []; 
     }
