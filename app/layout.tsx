0f3f8e9f31ae7ad5c2bc07ffeeee49e02a369115
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-25 06:02:25 -0800

    add script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Script from "next/script";
 import { getURL, isProd } from "@/lib/env";
 
-
 const defaultUrl = getURL();
 
 export const metadata = {
@@ -40,7 +39,10 @@ export default function RootLayout({
       </head>
       {isProd() && (
         <>
-          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-02CVJFYJG2" />
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-02CVJFYJG2"
+          />
           <Script strategy="afterInteractive" id="google-analytics">
             {`
               window.dataLayer = window.dataLayer || [];
@@ -48,6 +50,15 @@ export default function RootLayout({
               gtag('js', new Date());
               gtag('config', 'G-02CVJFYJG2');
             `}
+          </Script>
+          <Script type="text/javascript">
+            {`
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "l7bra27awx");
+              `}
           </Script>
         </>
       )}
