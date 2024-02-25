d158237de4c20b0ceb32598f4ecae9cda369ee5c
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-24 21:17:04 -0800

    fix back button

--- components/events/EventCardNav.tsx
+++ components/events/EventCardNav.tsx
@@ -1,19 +1,27 @@
+"use client";
 import { ChevronLeft } from "lucide-react";
 import { Button } from "../ui/button";
-import Link from "next/link";
 import CopyButton from "./CopyButton";
+import { useRouter } from "next/navigation";
 
 export default function EventCardNav() {
+  const router = useRouter();
+  const samePrevDomain = document.referrer.includes("localhost:3000");
 
+  const onBack = () => {
+    if (samePrevDomain) {
+      router.back();
+    } else {
+      router.push("/events");
+    }
+  }
+  
   return (
     <div className="flex justify-between items-center mb-4">
-      <Link href="/events" passHref>
-        <Button variant="outline" size="icon">
-          <ChevronLeft className="h-4 w-4" />
-        </Button>
-      </Link>
+      <Button variant="outline" size="icon" onClick={onBack}>
+        <ChevronLeft className="h-4 w-4" />
+      </Button>
       <CopyButton />
-
     </div>
   );
 }
