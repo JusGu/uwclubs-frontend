35fee3c32be94a86e8a417f37eb5008e010e805a
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-26 10:16:48 -0800

    fix script

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
         <link rel="icon" href="favicon.ico" />
         {isProd() && (
           <>
+            <Script strategy="afterInteractive" src="/clarity.js" />
             <Script
               strategy="afterInteractive"
               src="https://www.googletagmanager.com/gtag/js?id=G-02CVJFYJG2"
@@ -49,15 +50,6 @@ export default function RootLayout({
               gtag('js', new Date());
               gtag('config', 'G-02CVJFYJG2');
             `}
-            </Script>
-            <Script type="text/javascript">
-              {`
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "l7bra27awx");
-              `}
             </Script>
           </>
         )}
