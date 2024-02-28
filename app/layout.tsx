5ef3ae6988512dde14a3323b4808f4b23d45e047
Justin Gu <77459359+JusGu@users.noreply.github.com>
2024-02-28 18:54:05 -0500

    added gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,18 @@ export default function RootLayout({
         {isProd() && (
           <>
             <Script
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-N7WRVC5T');`,
+              }}
+            />
+
+            <Script
               id="ms_clarity"
               strategy="afterInteractive"
               dangerouslySetInnerHTML={{
@@ -67,6 +79,14 @@ export default function RootLayout({
         )}
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N7WRVC5T"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
