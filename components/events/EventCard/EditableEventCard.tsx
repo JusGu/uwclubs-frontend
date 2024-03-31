9e60e0058e639419250a714f027434f4843826fb
George Shao <GeorgeShao123@gmail.com>
2024-03-31 03:48:07 -0400

    refactor: Migrate data fetching from editable event card to event edit page component.

--- components/events/EventCard/EditableEventCard.tsx
+++ components/events/EventCard/EditableEventCard.tsx
@@ -5,9 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { createClient } from "@/utils/supabase/server";
 import { IEvent } from "@/app/events/models";
-import { cookies } from "next/headers";
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -16,40 +14,13 @@ import { MapPin } from "lucide-react";
 import { formatTimePicker } from "@/lib/datetime";
 
 export default async function EditableEventCard({
-  event_id,
+  event,
 }: {
-  event_id: string;
+  event: any;
 }) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const { data } = await supabase
-    .from("events")
-    .select(
-      `
-      id,
-      title,
-      start_time,
-      end_time,
-      location,
-      description,
-      guild_id,
-      channel_id,
-      message_id,
-      guilds ( short_name, description )
-      `
-    )
-    .eq("message_id", event_id)
-    .is("deleted_at", null);
-
-  //@ts-ignore
-  const event = data[0] as event;
-
   const getMessageLink = (event: IEvent) => {
     return `discord://discord.com/channels/${event.guild_id}/${event.channel_id}/${event.message_id}`;
   };
-  if (!event) {
-    return <div>Event not found</div>;
-  }
 
   return (
     <Card>
